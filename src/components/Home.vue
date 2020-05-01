aaf80e7ceb43f5308406ffccb5b07f4e47fb7d66
Maciej Faron <maciejfaron@o2.pl>
2020-05-02 01:29:32 +0200

    add logout button and authorization

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <button type="button" @click="logout()">Wyloguj</button>
+</template>
+
+<script>
+import firebase from "firebase";
+import router from '../router.js';
+
+export default {
+  name: "Home",
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => router.replace("/"));
+    }
+  }
+};
+</script>
