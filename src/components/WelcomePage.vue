f069c6190001ea99a221ce6520909435f2036aa1
Maciej Faron <maciejfaron@o2.pl>
2020-05-01 19:34:40 +0200

    add first route

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container fluid>
+    <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+      <p>For more information visit website</p>
+      <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "WelcomePage"
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
+
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: Routes
+})
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
