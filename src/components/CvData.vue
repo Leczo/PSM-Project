f831f4ba1929ab86892f0b8a3da8a91d3d391258
Leczo <maciek.czarn@gmail.com>
2020-05-03 01:25:15 +0200

    Components CvData and PersonalData

---
+++ src/components/CvData.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Twoje Dane:</h2>
+    <div>
+      <b-form-group label="Individual radios">
+        <b-form-radio v-model="selected" name="some-radios" value="1"
+          >Szablon 1</b-form-radio
+        >
+        <b-form-radio v-model="selected" name="some-radios" value="2"
+          >Szablon 2</b-form-radio
+        >
+      </b-form-group>
+
+      <div class="mt-3">
+        Selected: <strong>{{ selected }}</strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CvData",
+  data() {
+    return {
+      selected: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/PersonalData.vue
@@ -0,0 +1,51 @@
+<template>
+  <form role="form">
+    <div>
+      <div class="form-group">
+        <label>Imię</label>
+        <input class="form-control" placeholder="np. Jan" />
+      </div>
+      <div class="form-group">
+        <label>Nazwisko</label>
+        <input type="text" placeholder="np. Kowalski" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label>Numer telefonu</label>
+        <input
+          type="text"
+          placeholder="w formacie 000-000-000"
+          class="form-control"
+        />
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="np jan.kowalski@mail.pl"
+          class="form-control"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Zapisz</button>
+    </div>
+  </form>
+</template>
+
+<script>
+//import db from "firebase";
+
+export default {
+  name: "PersonalData",
+  data() {
+    return null;
+  },
+  components: {},
+  methods: {
+    saveEdForm: function() {
+      return null;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
