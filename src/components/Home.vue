f863f341c6dc13af901ea290c71fef70b6d60b78
Leczo <maciek.czarn@gmail.com>
2020-05-02 21:40:54 +0200

    Implemented Froms for Job Language and Education

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,79 +1,99 @@
 <template>
-  <b-jumbotron id="sth">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@3/distr/fira_code.css" />
-    <b-navbar
-      toggleable
-      style="  border-top-left-radius: 20px; border-top-right-radius: 20px;"
-      fixed="bottom"
-      sticky="true"
-      type="dark"
-      variant="dark"
-    >
-      <b-navbar-brand id="Brand">
-        <strong>Cv Generator</strong>
-      </b-navbar-brand>
+  <b-jumbotron id="jumbotron">
+    <div id="main-container">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@3/distr/fira_code.css"
+      />
+      <b-navbar
+        toggleable
+        style="  border-top-left-radius: 20px; border-top-right-radius: 20px;"
+        fixed="bottom"
+        sticky="true"
+        type="dark"
+        variant="dark"
+      >
+        <b-navbar-brand id="Brand">
+          <strong>Cv Generator</strong>
+        </b-navbar-brand>
 
-      <b-dropdown right text="Right align" variant="secondary">
-        <template v-slot:button-content>
-          <b-icon icon="power" aria-hidden="true"></b-icon>
-        </template>
-        <b-dropdown-item @click="logout()">Wyloguj</b-dropdown-item>
-      </b-dropdown>
-    </b-navbar>
-    <b-card no-body>
-      <b-tabs card>
-        <b-tab title="Główna" active>
-          <template v-slot:title>
-            <b-icon icon="pencil-square" title="d" aria-hidden="true"></b-icon>
-            <span>Główna</span>
+        <b-dropdown right text="Right align" variant="secondary">
+          <template v-slot:button-content>
+            <b-icon icon="power" aria-hidden="true"></b-icon>
           </template>
-          <b-card-text>Tab contents 1</b-card-text>
-        </b-tab>
-        <b-tab title="Uzupełnij Dane">
-          <template v-slot:title>
-            <b-icon icon="pencil-square" title="d" aria-hidden="true"></b-icon>
-            <span>Uzupełnij Dane</span>
-          </template>
-          <b-card-text>
-            <div>
+          <b-dropdown-item @click="logout()">Wyloguj</b-dropdown-item>
+        </b-dropdown>
+      </b-navbar>
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Główna" active>
+            <template v-slot:title>
+              <b-icon icon="house-fill" title="d" aria-hidden="true"></b-icon>
+              <span> Główna</span>
+            </template>
+            <b-card-text>Tab contents 1</b-card-text>
+          </b-tab>
+          <b-tab title="Uzupełnij Dane">
+            <template v-slot:title>
+              <b-icon
+                icon="pencil-square"
+                title="d"
+                aria-hidden="true"
+              ></b-icon>
+              <span> Uzupełnij Dane</span>
+            </template>
+            <b-card-text>
               <div>
-                <b-card no-body>
-                  <b-tabs justified variant="secondary" card vertical nav-wrapper-class="w-50">
-                    <b-tab title="Edukacja" active>
-                      <b-card-text>Edukacja</b-card-text>
-                      <EducationForm />
-                    </b-tab>
-                    <b-tab title="Praca">
-                      <b-card-text>Praca</b-card-text>
-                    </b-tab>
-                    <b-tab title="Języki">
-                      <b-card-text>Języki</b-card-text>
-                    </b-tab>
-                  </b-tabs>
-                </b-card>
+                <div>
+                  <b-card no-body>
+                    <b-tabs
+                      justified
+                      variant="secondary"
+                      card
+                      vertical
+                      nav-wrapper-class="w-50"
+                    >
+                      <b-tab title="Edukacja" active>
+                        <b-card-text><strong>Edukacja</strong></b-card-text>
+                        <EducationForm />
+                      </b-tab>
+                      <b-tab title="Praca">
+                        <b-card-text><strong>Praca</strong></b-card-text>
+                        <JobForm />
+                      </b-tab>
+                      <b-tab title="Języki">
+                        <b-card-text><strong>Praca</strong></b-card-text>
+                        <LanguageForm />
+                      </b-tab>
+                    </b-tabs>
+                  </b-card>
+                </div>
               </div>
-            </div>
-          </b-card-text>
-        </b-tab>
+            </b-card-text>
+          </b-tab>
 
-        <b-tab te="Zapisane CV">
-          <template v-slot:title>
-            <b-icon icon="folder-fill" title="d" aria-hidden="true"></b-icon>
-            <span>Upload</span>
-          </template>
-          <FileUpload />
-          <b-card-text>Tutaj File z bootstrapa</b-card-text>
-        </b-tab>
-      </b-tabs>
-    </b-card>
-    <b-navbar
-      toggleable
-      style="  border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;"
-      fixed="bottom"
-      sticky="true"
-      type="dark"
-      variant="dark"
-    ></b-navbar>
+          <b-tab te="Zapisane CV">
+            <template v-slot:title>
+              <b-icon
+                icon="cloud-download"
+                title="d"
+                aria-hidden="true"
+              ></b-icon>
+              <span> Zapisane</span>
+            </template>
+            <FileUpload />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+      <b-navbar
+        toggleable
+        style="  border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;"
+        fixed="bottom"
+        sticky="true"
+        type="dark"
+        variant="dark"
+      ></b-navbar>
+    </div>
   </b-jumbotron>
 </template>
 
@@ -82,12 +102,16 @@ import firebase from "firebase";
 import router from "../router.js";
 import FileUpload from "./FileUpload.vue";
 import EducationForm from "./EducationForm.vue";
+import JobForm from "./JobForm.vue";
+import LanguageForm from "./LanguageForm.vue";
 
 export default {
   name: "Home",
   components: {
     FileUpload,
-    EducationForm
+    EducationForm,
+    JobForm,
+    LanguageForm,
   },
   methods: {
     logout() {
@@ -95,16 +119,22 @@ export default {
         .auth()
         .signOut()
         .then(() => router.replace("/"));
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-#sth {
+#jumbotron {
   min-height: 100vh;
   margin: 0%;
   flex-direction: column;
+  justify-content: center;
+}
+#main-container {
+  width: 80%;
+  margin: 0 auto;
+  display: block;
   justify-content: center;
 }
 #Brand {
