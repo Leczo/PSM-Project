1c446104da151ed460e1f55a7cebe7ea51856a82
Leczo <maciek.czarn@gmail.com>
2020-05-03 10:05:16 +0200

    nav appearing fix

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="app">
-    <Nav></Nav>
     <router-view></router-view>
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Footer from "./components/Footer.vue";
-import Nav from "./components/Nav.vue";
-
 export default {
-  components: { Footer, Nav },
+  components: {},
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,177 +1,183 @@
 <template>
-  <b-jumbotron fluid id="jumbotron">
-    <div id="main-container">
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@3/distr/fira_code.css"
-      />
+  <div>
+    <Nav></Nav>
+    <b-jumbotron fluid id="jumbotron">
+      <div id="main-container">
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/tonsky/FiraCode@3/distr/fira_code.css"
+        />
 
-      <b-card no-body>
-        <b-tabs card>
-          <b-tab title="Główna" active>
-            <template v-slot:title>
-              <b-icon icon="house-fill" title="d" aria-hidden="true"></b-icon>
-              <span></span>
-            </template>
-            <b-card-text>
-              <div class="cvData">
-                <h2 v-if="dataNotAdded">Zacznij od wprowadzenia danych!</h2>
-                <h2 v-else>
-                  <strong>Informacje o tobie:</strong>
-                </h2>
-                <!-- Personal data -->
-                <div
-                  v-if="!dataNotAdded && personalDataAdded"
-                  id="personalData"
-                >
-                  <h3>Dane personalne:</h3>
-                  <ul>
-                    <li
-                      v-if="
-                        cvData.personalData.name != '' &&
-                          cvData.personalData.surname != ''
-                      "
-                    >
-                      Imię i nazwisko: {{ cvData.personalData.name }}
-                      {{ cvData.personalData.surname }}
-                    </li>
-                    <li v-if="cvData.personalData.phoneNumber != ''">
-                      Numer telefonu: {{ cvData.personalData.phoneNumber }}
-                    </li>
-                    <li v-if="cvData.personalData.email != ''">
-                      Adres email: {{ cvData.personalData.email }}
-                    </li>
-                  </ul>
-                </div>
-                <!-- Edu list -->
-                <div v-if="cvData.education.length > 0">
-                  <h3>Edukacja:</h3>
-                  <ul
-                    class="info-list"
-                    v-for="(edu, index) in cvData.education"
-                    :key="edu.universityName"
+        <b-card no-body>
+          <b-tabs card>
+            <b-tab title="Główna" active>
+              <template v-slot:title>
+                <b-icon icon="house-fill" title="d" aria-hidden="true"></b-icon>
+                <span></span>
+              </template>
+              <b-card-text>
+                <div class="cvData">
+                  <h2 v-if="dataNotAdded">Zacznij od wprowadzenia danych!</h2>
+                  <h2 v-else>
+                    <strong>Informacje o tobie:</strong>
+                  </h2>
+                  <!-- Personal data -->
+                  <div
+                    v-if="!dataNotAdded && personalDataAdded"
+                    id="personalData"
                   >
-                    <li>Nazwa szkoły/uczelni: {{ edu.universityName }}</li>
-                    <li>Profil: {{ edu.major }}</li>
-                    <li>Miesiąc/Rok rozpoczęcia: {{ edu.startDate }}</li>
-                    <li>Miesiąc/Rok zakończenia: {{ edu.endDate }}</li>
-                    <b-button
-                      variant="outline-primary"
-                      @click="removeData(index)"
-                      >Usuń tę informację</b-button
+                    <h3>Dane personalne:</h3>
+                    <ul>
+                      <li
+                        v-if="
+                          cvData.personalData.name != '' &&
+                            cvData.personalData.surname != ''
+                        "
+                      >
+                        Imię i nazwisko: {{ cvData.personalData.name }}
+                        {{ cvData.personalData.surname }}
+                      </li>
+                      <li v-if="cvData.personalData.phoneNumber != ''">
+                        Numer telefonu: {{ cvData.personalData.phoneNumber }}
+                      </li>
+                      <li v-if="cvData.personalData.email != ''">
+                        Adres email: {{ cvData.personalData.email }}
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- Edu list -->
+                  <div v-if="cvData.education.length > 0">
+                    <h3>Edukacja:</h3>
+                    <ul
+                      class="info-list"
+                      v-for="(edu, index) in cvData.education"
+                      :key="edu.universityName"
                     >
-                    <b-button @click="pushData()">Zapisz Dane</b-button>
-                  </ul>
-                </div>
-                <!-- Work list -->
-                <div v-if="cvData.work.length > 0">
-                  <h3>Praca:</h3>
-                  <ul
-                    class="info-list"
-                    v-for="(work, index) in cvData.work"
-                    :key="work.position"
-                  >
-                    <li>Nazwa firmy: {{ work.companyName }}</li>
-                    <li>Stanowisko: {{ work.position }}</li>
-                    <b-button
-                      variant="outline-primary"
-                      @click="removeDataFromWork(index)"
-                      >Usuń tę informację</b-button
+                      <li>Nazwa szkoły/uczelni: {{ edu.universityName }}</li>
+                      <li>Profil: {{ edu.major }}</li>
+                      <li>Miesiąc/Rok rozpoczęcia: {{ edu.startDate }}</li>
+                      <li>Miesiąc/Rok zakończenia: {{ edu.endDate }}</li>
+                      <b-button
+                        variant="outline-primary"
+                        @click="removeData(index)"
+                        >Usuń tę informację</b-button
+                      >
+                      <b-button @click="pushData()">Zapisz Dane</b-button>
+                    </ul>
+                  </div>
+                  <!-- Work list -->
+                  <div v-if="cvData.work.length > 0">
+                    <h3>Praca:</h3>
+                    <ul
+                      class="info-list"
+                      v-for="(work, index) in cvData.work"
+                      :key="work.position"
                     >
-                    <hr class="my-4" />
-                  </ul>
-                </div>
-                <!-- Languages list -->
-                <div v-if="cvData.languages.length > 0">
-                  <h3>Znajomośc języków:</h3>
-                  <ul
-                    class="info-list"
-                    v-for="(lang, index) in cvData.languages"
-                    :key="lang.language"
-                  >
-                    <li>Język: {{ lang.language }}</li>
-                    <li>Poziom: {{ lang.level }}</li>
-                    <b-button
-                      variant="outline-primary"
-                      @click="removeDataFromLanguages(index)"
-                      >Usuń tę informację</b-button
+                      <li>Nazwa firmy: {{ work.companyName }}</li>
+                      <li>Stanowisko: {{ work.position }}</li>
+                      <b-button
+                        variant="outline-primary"
+                        @click="removeDataFromWork(index)"
+                        >Usuń tę informację</b-button
+                      >
+                      <hr class="my-4" />
+                    </ul>
+                  </div>
+                  <!-- Languages list -->
+                  <div v-if="cvData.languages.length > 0">
+                    <h3>Znajomośc języków:</h3>
+                    <ul
+                      class="info-list"
+                      v-for="(lang, index) in cvData.languages"
+                      :key="lang.language"
                     >
-                  </ul>
+                      <li>Język: {{ lang.language }}</li>
+                      <li>Poziom: {{ lang.level }}</li>
+                      <b-button
+                        variant="outline-primary"
+                        @click="removeDataFromLanguages(index)"
+                        >Usuń tę informację</b-button
+                      >
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </b-card-text>
-          </b-tab>
-          <b-tab title="Uzupełnij Dane">
-            <template v-slot:title>
-              <b-icon
-                icon="pencil-square"
-                title="d"
-                aria-hidden="true"
-              ></b-icon>
-              <span>Uzupełnij Dane</span>
-            </template>
-            <b-card-text>
-              <div>
+              </b-card-text>
+            </b-tab>
+            <b-tab title="Uzupełnij Dane">
+              <template v-slot:title>
+                <b-icon
+                  icon="pencil-square"
+                  title="d"
+                  aria-hidden="true"
+                ></b-icon>
+                <span>Uzupełnij Dane</span>
+              </template>
+              <b-card-text>
                 <div>
-                  <b-card no-body>
-                    <b-tabs
-                      justified
-                      variant="secondary"
-                      card
-                      vertical
-                      nav-wrapper-class="w-50"
-                    >
-                      <b-tab alingn="left" title="Dane Osobowe" active>
-                        <b-card-text>
-                          <strong>Dane Osobowe</strong>
-                        </b-card-text>
-                        <PersonalData v-on:saveData="updateData"></PersonalData>
-                      </b-tab>
+                  <div>
+                    <b-card no-body>
+                      <b-tabs
+                        justified
+                        variant="secondary"
+                        card
+                        vertical
+                        nav-wrapper-class="w-50"
+                      >
+                        <b-tab alingn="left" title="Dane Osobowe" active>
+                          <b-card-text>
+                            <strong>Dane Osobowe</strong>
+                          </b-card-text>
+                          <PersonalData
+                            v-on:saveData="updateData"
+                          ></PersonalData>
+                        </b-tab>
 
-                      <b-tab title="Edukacja">
-                        <b-card-text>
-                          <strong>Edukacja</strong>
-                        </b-card-text>
-                        <EducationForm
-                          v-on:saveData="saveEducationData"
-                        ></EducationForm>
-                      </b-tab>
-                      <b-tab title="Praca">
-                        <b-card-text>
-                          <strong>Praca</strong>
-                        </b-card-text>
-                        <JobForm v-on:saveData="saveJobForm"></JobForm>
-                      </b-tab>
-                      <b-tab title="Języki">
-                        <b-card-text>
-                          <strong>Języki</strong>
-                        </b-card-text>
-                        <LanguageForm
-                          v-on:saveData="saveLanguageData"
-                        ></LanguageForm>
-                      </b-tab>
-                    </b-tabs>
-                  </b-card>
+                        <b-tab title="Edukacja">
+                          <b-card-text>
+                            <strong>Edukacja</strong>
+                          </b-card-text>
+                          <EducationForm
+                            v-on:saveData="saveEducationData"
+                          ></EducationForm>
+                        </b-tab>
+                        <b-tab title="Praca">
+                          <b-card-text>
+                            <strong>Praca</strong>
+                          </b-card-text>
+                          <JobForm v-on:saveData="saveJobForm"></JobForm>
+                        </b-tab>
+                        <b-tab title="Języki">
+                          <b-card-text>
+                            <strong>Języki</strong>
+                          </b-card-text>
+                          <LanguageForm
+                            v-on:saveData="saveLanguageData"
+                          ></LanguageForm>
+                        </b-tab>
+                      </b-tabs>
+                    </b-card>
+                  </div>
                 </div>
-              </div>
-            </b-card-text>
-          </b-tab>
+              </b-card-text>
+            </b-tab>
 
-          <b-tab te="Zapisane CV">
-            <template v-slot:title>
-              <b-icon
-                icon="cloud-download"
-                title="d"
-                aria-hidden="true"
-              ></b-icon>
-              <span>Zapisane</span>
-            </template>
-            <FileUpload />
-          </b-tab>
-        </b-tabs>
-      </b-card>
-    </div>
-  </b-jumbotron>
+            <b-tab te="Zapisane CV">
+              <template v-slot:title>
+                <b-icon
+                  icon="cloud-download"
+                  title="d"
+                  aria-hidden="true"
+                ></b-icon>
+                <span>Zapisane</span>
+              </template>
+              <FileUpload />
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </div>
+    </b-jumbotron>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
@@ -182,6 +188,8 @@ import LanguageForm from "./LanguageForm.vue";
 import PersonalData from "./PersonalData.vue";
 import firebase from "firebase";
 import router from "../router.js";
+import Nav from "./Nav.vue";
+import Footer from "./Footer.vue";
 
 export default {
   name: "Home",
@@ -191,6 +199,8 @@ export default {
     JobForm,
     LanguageForm,
     PersonalData,
+    Nav,
+    Footer,
   },
   created() {
     const db = firebase.firestore();
